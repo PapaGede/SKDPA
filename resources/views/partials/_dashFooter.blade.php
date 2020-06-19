028f9a6faaf0825e35081edbd5b31568500e6039
PapaGede <naqhouamy@gmail.com>
2020-06-19 23:04:09 +0000

    added porter details in footer

--- resources/views/partials/_dashFooter.blade.php
+++ resources/views/partials/_dashFooter.blade.php
@@ -2,6 +2,7 @@
     <div class="container-fluid">
         <div class="d-flex align-items-center justify-content-between small">
             <div class="text-muted">Copyright &copy; SKDPA 2019</div>
+            <div class="text-muted">Currently logged in as:Porter {{Auth::user()->name}} ({{Auth::user()->email}})</div>
             <div>
                 <a href="#">Privacy Policy</a>
                 &middot;
