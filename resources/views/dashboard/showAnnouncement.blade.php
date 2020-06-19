951a8e9f2f9195c5aa4bce3d0920e140c3060940
PapaGede <naqhouamy@gmail.com>
2020-06-19 23:03:30 +0000

    Made a few changes to the announcement view

--- resources/views/dashboard/showAnnouncement.blade.php
+++ resources/views/dashboard/showAnnouncement.blade.php
@@ -8,12 +8,12 @@
 
 <div>
     @foreach ($all_announce as $item)
-    <div class="jumbotron" style="padding:2em 1em">
+    <div class="jumbotron" style="padding:.15em 1em">
         <div class="post">
             <div>
                 <h4><img src="images/announcement.png" height="20px"/> {{$item['title']}}<p class="lead text-muted" style="display: inline;font-size:15px"><span style="font-weight: bold">--Porter {{$item['sender']}}</span></p></h4>
             </div>
-        <p class="lead">{{$item['message']}}</p>
+        <p class="lead" style="font-size: 18px">{{$item['message']}}</p>
         <p style="text-align: right"> Created at: {{$item['date']}} {{$item['time']}}</p>
         </div>
     </div>
