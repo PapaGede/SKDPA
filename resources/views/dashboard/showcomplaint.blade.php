51a130827fbe1214e55d96700ab1a8257b8ef73d
PapaGede <naqhouamy@gmail.com>
2020-06-30 20:16:06 +0000

    Final Push

--- resources/views/dashboard/showcomplaint.blade.php
+++ resources/views/dashboard/showcomplaint.blade.php
@@ -2,8 +2,35 @@
 @section('content')
 @foreach ($all_complaint as $item)
 @if ($item['header']=="Electricity Issues")
-<div class="jumbotron" style="padding:2em 1em">
-        <h6><img src="images/red.png" height="22px"> {{$item['header']}}</h6>
+<div class="jumbotron" style="padding:1em 1em">
+        <h6><img src="images/red.png" height="22px"> {{$item['header']}} {{$item['sender']}}</h6>
+        <p class="lead">{{$item['message']}}</p>
+        <p style="text-align: right">{{$item['time']}}</p>
+
+</div>
+@endif
+
+@if ($item['header']=="Water Issues")
+<div class="jumbotron" style="padding:1em 1em">
+        <h6><img src="images/water.png" height="22px"> {{$item['header']}} by {{$item['sender']}}</h6>
+        <p class="lead">{{$item['message']}}</p>
+        <p style="text-align: right">{{$item['time']}}</p>
+
+</div>
+@endif
+
+@if ($item['header']=="Furniture Issues")
+<div class="jumbotron" style="padding:1em 1em">
+        <h6><img src="images/furniture.png" height="22px"> {{$item['header']}} by {{$item['sender']}}</h6>
+        <p class="lead">{{$item['message']}}</p>
+        <p style="text-align: right">{{$item['time']}}</p>
+
+</div>
+@endif
+
+@if ($item['header']=="Plumbing Issues")
+<div class="jumbotron" style="padding:1em 1em">
+        <h6><img src="images/furniture.png" height="22px"> {{$item['header']}} by {{$item['sender']}}</h6>
         <p class="lead">{{$item['message']}}</p>
         <p style="text-align: right">{{$item['time']}}</p>
 
